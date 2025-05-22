47f8a49f8bf99df14cca12149de53efa9ee191c1
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-05-22 18:00:58 +0300

    удален лишний код

--- src/main/java/com/example/usermanagement/config/SecurityConfig.java
+++ src/main/java/com/example/usermanagement/config/SecurityConfig.java
@@ -6,12 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.config.Customizer;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,18 +24,5 @@ public class SecurityConfig {
                 .anyRequest().authenticated());
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:8081"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
